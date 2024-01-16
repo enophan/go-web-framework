0d120ea7296dc09d9c83f2965f2b4dedcc9bee86
enophan <zhaooooooyifan@qq.com>
2024-01-16 12:18:16 +0800

    refactor: 提取路由相关的方法和结构

--- zolor.go
+++ zolor.go
@@ -1,32 +1,26 @@
 package zolor
 
 import (
-	"fmt"
 	"net/http"
 )
 
-type HandlerFunc func(http.ResponseWriter, *http.Request)
+type HandlerFunc func(*Context)
 
 type Engine struct {
-	router map[string]HandlerFunc
+	router *router
 }
 
 func New() *Engine {
-	return &Engine{router: make(map[string]HandlerFunc)}
+	return &Engine{router: newRouter()}
 }
 
 func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
-	key := method + "-" + pattern
-	engine.router[key] = handler
+	engine.router.addRoute(method, pattern, handler)
 }
 
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	key := req.Method + "-" + req.URL.Path
-	if handler, ok := engine.router[key]; ok {
-		handler(w, req)
-	} else {
-		fmt.Fprintf(w, "404")
-	}
+	c := newContext(w, *req)
+	engine.router.handle(c)
 }
 
 func (engine *Engine) Run(addr string) (err error) {
